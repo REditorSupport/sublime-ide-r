304538bd7985836da74a0329af93bedf62d9d8fd
Randy Lai <randy.cs.lai@gmail.com>
2018-09-07 16:55:15 -0400

    use expand_variables to expand file path

--- ride/render.py
+++ ride/render.py
@@ -3,47 +3,57 @@ import sublime_plugin
 import os
 
 from .settings import ride_settings
+from .utils import expand_variables
 
 
-class RideRenderRmarkdownCommand(sublime_plugin.TextCommand):
+class RideRenderRmarkdownCommand(sublime_plugin.WindowCommand):
     def is_enabled(self):
-        return self.view.settings().get("syntax").endswith("R Markdown.sublime-syntax")
+        view = self.window.active_view()
+        return view.settings().get("syntax").endswith("R Markdown.sublime-syntax")
 
-    def run(self, edit):
+    def run(self):
         cmd = "rmarkdown::render(\"$file\", encoding = \"UTF-8\")"
+        extracted_variables = self.window.extract_variables()
+        cmd = expand_variables(cmd, extracted_variables)
         kwargs = {}
         kwargs["cmd"] = [ride_settings.r_binary(), "--slave", "-e", cmd]
-        kwargs["working_dir"] = os.path.dirname(self.view.file_name())
+        kwargs["working_dir"] = os.path.dirname(self.window.active_view().file_name())
         kwargs["env"] = {"PATH": ride_settings.custom_env("PATH")}
-        kwargs = sublime.expand_variables(kwargs, self.view.window().extract_variables())
-        self.view.window().run_command("exec", kwargs)
+        kwargs = sublime.expand_variables(kwargs, self.window.extract_variables())
+        self.window.run_command("exec", kwargs)
 
 
-class RideSweaveRnwCommand(sublime_plugin.TextCommand):
+class RideSweaveRnwCommand(sublime_plugin.WindowCommand):
     def is_enabled(self):
-        return self.view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
+        view = self.window.active_view()
+        return view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
 
     def run(self, edit):
         cmd = ("""Sweave(\"$file\")\n"""
                """tools::texi2dvi(\"$file_base_name.tex\", pdf = TRUE)""")
+        extracted_variables = self.window.extract_variables()
+        cmd = expand_variables(cmd, extracted_variables)
         kwargs = {}
         kwargs["cmd"] = [ride_settings.r_binary(), "--slave", "-e", cmd]
-        kwargs["working_dir"] = os.path.dirname(self.view.file_name())
+        kwargs["working_dir"] = os.path.dirname(self.window.active_view().file_name())
         kwargs["env"] = {"PATH": ride_settings.custom_env("PATH")}
-        kwargs = sublime.expand_variables(kwargs, self.view.window().extract_variables())
-        self.view.window().run_command("exec", kwargs)
+        kwargs = sublime.expand_variables(kwargs, self.window.extract_variables())
+        self.window.run_command("exec", kwargs)
 
 
-class RideKnitRnwCommand(sublime_plugin.TextCommand):
+class RideKnitRnwCommand(sublime_plugin.WindowCommand):
     def is_enabled(self):
-        return self.view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
+        view = self.window.active_view()
+        return view.settings().get("syntax").endswith("R Sweave.sublime-syntax")
 
     def run(self, edit):
         cmd = ("""knitr::knit(\"$file\", output=\"$file_base_name.tex\")\n"""
                """tools::texi2dvi(\"$file_base_name.tex\", pdf = TRUE)")""")
+        extracted_variables = self.window.extract_variables()
+        cmd = expand_variables(cmd, extracted_variables)
         kwargs = {}
         kwargs["cmd"] = [ride_settings.r_binary(), "--slave", "-e", cmd]
-        kwargs["working_dir"] = os.path.dirname(self.view.file_name())
+        kwargs["working_dir"] = os.path.dirname(self.window.active_view().file_name())
         kwargs["env"] = {"PATH": ride_settings.custom_env("PATH")}
-        kwargs = sublime.expand_variables(kwargs, self.view.window().extract_variables())
-        self.view.window().run_command("exec", kwargs)
+        kwargs = sublime.expand_variables(kwargs, self.window.extract_variables())
+        self.window.run_command("exec", kwargs)
--- ride/utils.py
+++ ride/utils.py
@@ -1,5 +1,7 @@
 import sublime
 
+import re
+
 if sublime.platform() == "windows":
     from winreg import OpenKey, QueryValueEx, HKEY_LOCAL_MACHINE, KEY_READ
 
@@ -12,4 +14,38 @@ def read_registry(key, valueex):
 def escape_dquote(cmd):
     cmd = cmd.replace('\\', '\\\\')
     cmd = cmd.replace('"', '\\"')
+    return cmd
+
+
+def escape_squote(cmd):
+    cmd = cmd.replace('\\', '\\\\')
+    cmd = cmd.replace("\'", "\'")
+    return cmd
+
+
+PATTERN = re.compile(r"""
+    (?P<quote>["'])
+    (?P<quoted_var>
+        \$ (?: [_a-z][_a-z0-9]*  | \{[^}]*\} )
+    )
+    (?P=quote)
+    |
+    (?P<var>
+        \$ (?: [_a-z][_a-z0-9]*  | \{[^}]*\} )
+    )
+""", re.VERBOSE)
+
+
+def expand_variables(cmd, extracted_variables):
+    def convert(m):
+        quote = m.group("quote")
+        if quote:
+            var = sublime.expand_variables(m.group("quoted_var"), extracted_variables)
+            if quote == "'":
+                return "'" + escape_squote(var) + "'"
+            else:
+                return '"' + escape_dquote(var) + '"'
+        else:
+            return sublime.expand_variables(m.group("var"), extracted_variables)
+    cmd = PATTERN.sub(convert, cmd)
     return cmd
