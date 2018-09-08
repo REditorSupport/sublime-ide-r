244c2e567f4e3c3c0a9b4830cd41c4b7ace0cd86
Randy Lai <randy.cs.lai@gmail.com>
2018-09-08 16:45:41 -0400

    move to ride_package_exec

--- ride/__init__.py
+++ ride/__init__.py
@@ -1,10 +1,11 @@
 from .main_menu import (
-    RideMainMenuListener, RidePackageExecCommand,
+    RideMainMenuListener,
     plugin_unloaded as main_menu_plugin_unloaded
 )
 from .render import RideRenderRmarkdownCommand, RideSweaveRnwCommand, RideKnitRnwCommand
 from .source_prompt import RideSourcePromptCommand
 from .extract_function import RideExtractFunctionCommand
+from .exec import RidePackageExecCommand
 from .lsp import (
     LspRLangPlugin, LspCqueryRPlugin,
     plugin_loaded as lsp_plugin_loaded
