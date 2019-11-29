3fc25d4af9e746c9d788d00d33e498db2b8c5f30
Randy Lai <randy.cs.lai@gmail.com>
2019-11-29 10:50:23 -0800

    rename file

--- patch/update-patch.sh
+++ patch/update-patch.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-
 diff -u <(curl -s -L https://raw.githubusercontent.com/sublimehq/Packages/759d6eed9b4beed87e602a23303a121c3a6c2fb3/Markdown/Markdown.sublime-syntax) ../R\ Markdown.sublime-syntax > R\ Markdown.patch
