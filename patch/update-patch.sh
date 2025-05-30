af8dce261aa4e013e41d2d0d8ef226fb1d21970e
deathaxe <16542113+deathaxe@users.noreply.github.com>
2025-05-31 01:33:29 +0200

    Fix R Markdown for Sublime Text 4200+ (#57)

--- patch/update-patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-diff -u <(curl -s -L https://raw.githubusercontent.com/sublimehq/Packages/7ab85554037733cdb54cf8e9c71171a1bbd0918b/Markdown/Markdown.sublime-syntax) ../R\ Markdown.sublime-syntax > R\ Markdown.patch
